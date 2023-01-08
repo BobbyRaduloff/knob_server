bcab8ee35ba015e0ff247977cffd28a32fa7e124
Boris Radulov <sc20br@leeds.ac.uk>
2023-01-09 00:30:51 +0200

    fuack

--- src/api/get-rev-registry.js
+++ src/api/get-rev-registry.js
@@ -5,29 +5,31 @@ export default async function (req, res) {
   try {
     await db_connect();
 
-    const cap = (x) => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase();
-    try {
-      if (req.body.cyrilic_name) {
-        const s = req.body.cyrilic_name.split(" ");
-        console.log(s);
-        req.body.cyrilic_name = cap(s[0]) + " " + cap(s[1]) + " " + cap(s[2]);
+    if (req.query) {
+      const cap = (x) => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase();
+      try {
+        if (req.query.cyrilic_name) {
+          const s = req.query.cyrilic_name.split(" ");
+          console.log(s);
+          req.query.cyrilic_name = cap(s[0]) + " " + cap(s[1]) + " " + cap(s[2]);
+        }
+        if (req.query.latin_name) {
+          const s = req.query.latin_name.split(" ");
+          req.query.latin_name = cap(s[0]) + " " + cap(s[1]) + " " + cap(s[2]);
+        }
+        if (req.query.cyrilic_city) {
+          req.query.cyrilic_city = cap(req.query.cyrilic_city);
+        }
+        if (req.query.latin_city) {
+          req.query.latin_city = cap(req.query.latin_city);
+        }
+      } catch {
+        res.status(400).json({ results: "Грешни данни!" });
+        return;
       }
-      if (req.body.latin_name) {
-        const s = req.body.latin_name.split(" ");
-        req.body.latin_name = cap(s[0]) + " " + cap(s[1]) + " " + cap(s[2]);
-      }
-      if (req.body.cyrilic_city) {
-        req.body.cyrilic_city = cap(req.body.cyrilic_city);
-      }
-      if (req.body.latin_city) {
-        req.body.latin_city = cap(req.body.latin_city);
-      }
-    } catch {
-      res.status(400).json({ results: "Грешни данни!" });
-      return;
     }
 
-    res.status(200).json({ results: await REV.find({ ...req.body }) });
+    res.status(200).json({ results: await REV.find({ ...req.query }) });
   } catch (e) {
     if (db_handle_error(e, res)) return;
   }
