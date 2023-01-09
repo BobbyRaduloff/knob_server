fb8c84f7948322397c358686288899e3ac9a0055
Boris Radulov <sc20br@leeds.ac.uk>
2023-01-10 00:15:36 +0200

    asd

--- src/api/get-companies.js
+++ src/api/get-companies.js
@@ -5,24 +5,39 @@ export default async function (req, res) {
   try {
     await db_connect();
 
-    if (req.body.name || req.body.city || req.body.certificate_number) {
+    if (req.query.name || req.query.city || req.query.certificate_number) {
       let query = {};
-      if (req.body.name) query.name = req.body.name;
-      if (req.body.city) query.city = req.body.city;
+      if (req.query.name) query.name = req.query.name;
+      if (req.query.city) query.city = req.query.city;
 
       let result = await Company.find(query)
         .populate("capacity")
         .populate("valuers")
         .populate("current_valid_certificate");
 
-      if (req.body.certificate_number) {
-        result = result.filter((x) => x.current_valid_certificate.certificate_number === req.body.certificate_number);
+      if (req.query.certificate_number) {
+        result = result.filter((x) => x.current_valid_certificate.certificate_number === req.query.certificate_number);
       }
 
       res.status(200).json({ results: result });
     } else {
       res.status(200).json({
-        results: await Company.find().populate("capacity").populate("valuers").populate("current_valid_certificate"),
+        results: await Company.find()
+          .populate("capacity")
+          .populate("valuers")
+          .populate("current_valid_certificate")
+          .populate({
+            path: "valuers",
+            populate: {
+              path: "current_valid_certificate",
+            },
+          })
+          .populate({
+            path: "valuers",
+            populate: {
+              path: "invalid_certificates",
+            },
+          }),
       });
     }
   } catch (e) {
--- src/api/get-users.js
+++ src/api/get-users.js
@@ -5,25 +5,25 @@ export default async function (req, res) {
   try {
     await db_connect();
 
-    if (req.body.name || req.body.city || req.body.certificate_number) {
+    if (req.query.name || req.query.city || req.query.certificate_number) {
       let r = await User.find().populate("invalid_certificates").populate("current_valid_certificate");
       r.forEach((x) => (x.password_hash = undefined));
 
-      if (req.body.name) {
+      if (req.query.name) {
         r = r.filter((x) => {
           const full_name = `${x.first_name} ${x.middle_name} ${x.last_name}`;
-          return full_name.includes(req.body.name);
+          return full_name.includes(req.query.name);
         });
       }
 
-      if (req.body.city) {
-        r = r.filter((x) => x.city == req.body.city);
+      if (req.query.city) {
+        r = r.filter((x) => x.city == req.query.city);
       }
 
-      if (req.body.certificate_number) {
+      if (req.query.certificate_number) {
         r = r.filter((x) => {
           if (!x.current_valid_certificate) return false;
-          return x.current_valid_certificate.certificate_number === req.body.certificate_number;
+          return x.current_valid_certificate.certificate_number === req.query.certificate_number;
         });
       }
 
@@ -38,29 +38,3 @@ export default async function (req, res) {
     if (db_handle_error(e, res)) return;
   }
 }
-
-// export default async function (req, res) {
-//   try {
-//     await db_connect();
-
-//     if (req.body.name || req.body.city || req.body.certificate_number) {
-
-//       let result = await Company.find(query)
-//         .populate("capacity")
-//         .populate("valuers")
-//         .populate("current_valid_certificate");
-
-//       if (req.body.certificate_number) {
-//         result = result.filter((x) => x.current_valid_certificate.certificate_number === req.body.certificate_number);
-//       }
-
-//       res.status(200).json({ results: result });
-//     } else {
-//       res.status(200).json({
-//         results: await Company.find().populate("capacity").populate("valuers").populate("current_valid_certificate"),
-//       });
-//     }
-//   } catch (e) {
-//     if (db_handle_error(e, res)) return;
-//   }
-// }
